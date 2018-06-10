a27a96e4670b202d4a63d8688938845ef4b39514
Filip <fhagno@yahoo.com>
2018-06-10 15:57:02 +0200

    Initial changes

---
+++ game-bulls_and_cows/Main.cpp
@@ -0,0 +1,14 @@
+// game-bulls_and_cows.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Bulls and Cows game \n";
+	system("pause");
+	return 0;
+}
