2cf94224c607080a3023e873a79e25b4f3fca620
Filip <fhagno@yahoo.com>
2018-06-17 16:15:25 +0200

    Added initial game loop

--- game-bulls_and_cows/Main.cpp
+++ game-bulls_and_cows/Main.cpp
@@ -7,22 +7,35 @@
 
 using namespace std;
 
+constexpr int WORLD_LENGTH = 5;
 const string GAME_PROMO = "Bulls and Cows - a genius word game";
-
+const int NUMBER_OF_TURNS = WORLD_LENGTH * 1;
 
 //prototypes
 void PrintIntro(string message);
-string GetGuessAndPrintBack();
+string GetGuess();
+void PrintToConsole(std::string &Guess);
 void PrintPromptMessage(int numberOfcharsInWord);
+void StartGame();
 void DrawCowPictureInAscii();
 
 int main()
 {
 	PrintIntro(GAME_PROMO);
-	GetGuessAndPrintBack();
-	
-	system("pause");
+	StartGame();
+
 	return 0;
+}
+
+void StartGame()
+{
+	string Guess = "";
+
+	for (int i = 0; i < NUMBER_OF_TURNS; i++) {
+		Guess = GetGuess();
+		PrintToConsole(Guess);
+		cout << endl;
+	}
 }
 
 void PrintPromptMessage(int numberOfcharsInWord)
@@ -30,23 +43,24 @@ void PrintPromptMessage(int numberOfcharsInWord)
 	cout << "Can you guess the " << numberOfcharsInWord << " letter isogram I am thinking off?" << endl;
 }
 
-string GetGuessAndPrintBack()
+string GetGuess()
 {
 	string Guess = "";
 	cout << "Enter your guess: ";
 	getline(cin, Guess);
-	cout << "Your guess was : " << Guess << endl;
 	return Guess;
+}
+
+void PrintToConsole(std::string &Guess)
+{
+	cout << "Your guess was : " << Guess << endl;
 }
 
 void PrintIntro(string message)
 {
-	constexpr int WORLD_LENGTH = 5;
 	cout << "Welcome to " << message << "! \n";
 	DrawCowPictureInAscii();
 	PrintPromptMessage(WORLD_LENGTH);
-
-	
 }
 
 void DrawCowPictureInAscii()
